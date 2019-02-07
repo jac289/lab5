7ddaea17fe27cfa5832c30b48691e83966d94636
Jaclyn Chin <jac289@ucsd.edu>
2019-02-07 10:47:24 -0800

    he

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,9 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index');
+  res.render('index', {
+    'name': 'Fake Friend',
+    'image': 'http://lorempixel.com/500/500/people',
+    'id': 'friend1'
+  });
 };
